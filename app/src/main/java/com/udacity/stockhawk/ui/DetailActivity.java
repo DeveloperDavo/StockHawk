0a801f2f2df1e2634d07ef18669e9e1d79c61f50
David <developerdavo@gmail.com>
2016-11-21 18:41:18 +0100

    Add chart with dummy data

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -5,9 +5,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.udacity.stockhawk.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.chart)
+    LineChart lineChart;
 
     public static Intent newIntent(Context context) {
         return new Intent(context, DetailActivity.class);
@@ -16,6 +29,25 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
+
+        YourData[] dataObjects = new YourData[]{new YourData(0, 0), new YourData(5, 3), new YourData(10, 10)};
+
+        List<Entry> entries = new ArrayList<>();
+
+        for (YourData data : dataObjects) {
+
+            // turn your data into Entry objects
+            entries.add(new Entry(data.getX(), data.getY()));
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+
+        LineData lineData = new LineData(dataSet);
+        lineChart.setData(lineData);
+        lineChart.invalidate(); // refresh
+
     }
 }
